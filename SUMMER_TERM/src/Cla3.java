7d8d303a5d891946c311f9c4754052227629d48c
lakshmikanth1008 <58290768+lakshmikanth1008@users.noreply.github.com>
2021-05-12 11:41:17 +0530

    command line arguments
    practice on command line arguments

---
+++ SUMMER_TERM/src/Cla3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Cla3
+{
+public static void main(String args[])
+{ 
+int x=Integer.parseInt(args[0]);
+int y=Integer.parseInt(args[1]);
+int s=x+y;
+System.out.println("The sum of "+args[0]+" and "+args[1]+" is "+s);
+}
+}
